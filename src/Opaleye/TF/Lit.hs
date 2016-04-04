150e3a6f7654176ae8abfe242d1509feeb5d17b6
Ollie Charles <ollie@ocharles.org.uk>
2016-04-04 15:30:30 +0100

    Add litMaybe

--- src/Opaleye/TF/Lit.hs
+++ src/Opaleye/TF/Lit.hs
@@ -6,6 +6,12 @@
 module Opaleye.TF.Lit where
 
 import Opaleye.TF.Expr
+import Opaleye.TF.Nullable
+import Prelude hiding (null)
 
 class Lit (pgType :: k) (haskellType :: *) | pgType -> haskellType where
   lit :: haskellType -> Expr pgType
+
+litMaybe :: Lit pg haskell
+         => Maybe haskell -> Expr ('Nullable pg)
+litMaybe = maybe null (toNullable . lit)
