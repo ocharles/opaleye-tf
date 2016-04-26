1039992d58e1003b5575f50da91109e95eff737e
Rehno Lindeque <errantkid@gmail.com>
2016-04-26 11:12:47 +0000

    Update default.nix with missing dependencies

--- default.nix
+++ default.nix
@@ -1,18 +1,16 @@
-{ mkDerivation, base, opaleye, postgresql-simple
-, product-profunctors, profunctors, stdenv, transformers
-, tasty, tasty-hunit
+{ mkDerivation, base, bytestring, opaleye, postgresql-simple
+, product-profunctors, profunctors, stdenv, tasty, tasty-hunit
+, text, time, transformers
 }:
 mkDerivation {
   pname = "opaleye-tf";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    base opaleye postgresql-simple product-profunctors profunctors
-    transformers
+    base bytestring opaleye postgresql-simple product-profunctors
+    profunctors text time transformers
   ];
-  testHaskellDepends = [
-    tasty tasty-hunit
-  ];
+  testHaskellDepends = [ base tasty tasty-hunit ];
   homepage = "https://github.com/ocharles/opaleye-tf";
   description = "A client library for Opaleye using type families";
   license = stdenv.lib.licenses.bsd3;
